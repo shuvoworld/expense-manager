a2919a989ea3e6305eb0aa1d22df6c8b96f66e43
Laravel Daily <info@laraveldaily.com>
2022-08-12 14:46:01 +0000

    QuickAdminPanel automatic commit

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -85,70 +85,6 @@ class HomeController
         }
 
         $settings3 = [
-            'chart_title'           => 'Last 90 Days Expense List',
-            'chart_type'            => 'latest_entries',
-            'report_type'           => 'group_by_date',
-            'model'                 => 'App\Models\Expense',
-            'group_by_field'        => 'date',
-            'group_by_period'       => 'day',
-            'aggregate_function'    => 'count',
-            'filter_field'          => 'created_at',
-            'filter_days'           => '90',
-            'group_by_field_format' => 'd/m/Y',
-            'column_class'          => 'col-md-6',
-            'entries_number'        => '20',
-            'fields'                => [
-                'date'     => '',
-                'category' => 'name',
-                'amount'   => '',
-            ],
-            'translation_key' => 'expense',
-        ];
-
-        $settings3['data'] = [];
-        if (class_exists($settings3['model'])) {
-            $settings3['data'] = $settings3['model']::latest()
-                ->take($settings3['entries_number'])
-                ->get();
-        }
-
-        if (!array_key_exists('fields', $settings3)) {
-            $settings3['fields'] = [];
-        }
-
-        $settings4 = [
-            'chart_title'           => 'Last 90 Days Deposit List',
-            'chart_type'            => 'latest_entries',
-            'report_type'           => 'group_by_date',
-            'model'                 => 'App\Models\Deposit',
-            'group_by_field'        => 'date',
-            'group_by_period'       => 'day',
-            'aggregate_function'    => 'count',
-            'filter_field'          => 'created_at',
-            'filter_days'           => '90',
-            'group_by_field_format' => 'd/m/Y',
-            'column_class'          => 'col-md-6',
-            'entries_number'        => '5',
-            'fields'                => [
-                'date'      => '',
-                'depositor' => 'name',
-                'amount'    => '',
-            ],
-            'translation_key' => 'deposit',
-        ];
-
-        $settings4['data'] = [];
-        if (class_exists($settings4['model'])) {
-            $settings4['data'] = $settings4['model']::latest()
-                ->take($settings4['entries_number'])
-                ->get();
-        }
-
-        if (!array_key_exists('fields', $settings4)) {
-            $settings4['fields'] = [];
-        }
-
-        $settings5 = [
             'chart_title'           => 'Last 30 Days Expense',
             'chart_type'            => 'number_block',
             'report_type'           => 'group_by_date',
@@ -165,27 +101,65 @@ class HomeController
             'translation_key'       => 'expense',
         ];
 
-        $settings5['total_number'] = 0;
-        if (class_exists($settings5['model'])) {
-            $settings5['total_number'] = $settings5['model']::when(isset($settings5['filter_field']), function ($query) use ($settings5) {
-                if (isset($settings5['filter_days'])) {
-                    return $query->where($settings5['filter_field'], '>=',
-                now()->subDays($settings5['filter_days'])->format('Y-m-d'));
+        $settings3['total_number'] = 0;
+        if (class_exists($settings3['model'])) {
+            $settings3['total_number'] = $settings3['model']::when(isset($settings3['filter_field']), function ($query) use ($settings3) {
+                if (isset($settings3['filter_days'])) {
+                    return $query->where($settings3['filter_field'], '>=',
+                now()->subDays($settings3['filter_days'])->format('Y-m-d'));
                 }
-                if (isset($settings5['filter_period'])) {
-                    switch ($settings5['filter_period']) {
+                if (isset($settings3['filter_period'])) {
+                    switch ($settings3['filter_period']) {
                 case 'week': $start = date('Y-m-d', strtotime('last Monday')); break;
                 case 'month': $start = date('Y-m') . '-01'; break;
                 case 'year': $start = date('Y') . '-01-01'; break;
             }
                     if (isset($start)) {
-                        return $query->where($settings5['filter_field'], '>=', $start);
+                        return $query->where($settings3['filter_field'], '>=', $start);
                     }
                 }
             })
-                ->{$settings5['aggregate_function'] ?? 'count'}($settings5['aggregate_field'] ?? '*');
+                ->{$settings3['aggregate_function'] ?? 'count'}($settings3['aggregate_field'] ?? '*');
         }
 
-        return view('home', compact('settings1', 'settings2', 'settings3', 'settings4', 'settings5'));
+        $settings4 = [
+            'chart_title'           => 'Last 30 Days Deposit',
+            'chart_type'            => 'number_block',
+            'report_type'           => 'group_by_date',
+            'model'                 => 'App\Models\Deposit',
+            'group_by_field'        => 'date',
+            'group_by_period'       => 'day',
+            'aggregate_function'    => 'sum',
+            'aggregate_field'       => 'amount',
+            'filter_field'          => 'created_at',
+            'filter_days'           => '30',
+            'group_by_field_format' => 'd/m/Y',
+            'column_class'          => 'col-md-3',
+            'entries_number'        => '5',
+            'translation_key'       => 'deposit',
+        ];
+
+        $settings4['total_number'] = 0;
+        if (class_exists($settings4['model'])) {
+            $settings4['total_number'] = $settings4['model']::when(isset($settings4['filter_field']), function ($query) use ($settings4) {
+                if (isset($settings4['filter_days'])) {
+                    return $query->where($settings4['filter_field'], '>=',
+                now()->subDays($settings4['filter_days'])->format('Y-m-d'));
+                }
+                if (isset($settings4['filter_period'])) {
+                    switch ($settings4['filter_period']) {
+                case 'week': $start = date('Y-m-d', strtotime('last Monday')); break;
+                case 'month': $start = date('Y-m') . '-01'; break;
+                case 'year': $start = date('Y') . '-01-01'; break;
+            }
+                    if (isset($start)) {
+                        return $query->where($settings4['filter_field'], '>=', $start);
+                    }
+                }
+            })
+                ->{$settings4['aggregate_function'] ?? 'count'}($settings4['aggregate_field'] ?? '*');
+        }
+
+        return view('home', compact('settings1', 'settings2', 'settings3', 'settings4'));
     }
 }
